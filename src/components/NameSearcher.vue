2acd3f3948c4595f3736293062ea54da77648181
Anton Kononov <korselmain@gmail.com>
2024-06-23 22:18:38 +0200

    Validate the input field to only accept letters and to not be smaller than 2 characters

--- src/components/NameSearcher.vue
+++ src/components/NameSearcher.vue
@@ -4,10 +4,10 @@
       <el-col>Check that your name is lucky today:</el-col>
     </el-row>
     <el-row>
-      <el-col><el-input class="nameinput" v-model="nameInput" placeholder="Input 'name'" /></el-col>
+      <el-col><el-input class="nameinput" v-model="nameInput" placeholder="Input 'name'" clearable type="text" :formatter="formatName" /></el-col>
     </el-row>
     <el-row>
-      <el-col><el-button type="success" round @click="submitName">Submit</el-button></el-col>
+      <el-col><el-button type="success" :disabled="isDisabled" round @click="submitName">Submit</el-button></el-col>
     </el-row>
   </div>
   <name-search-response></name-search-response>
@@ -31,15 +31,21 @@ export default {
       set(value) {
         this.setName(value)
       }
+    },
+    isDisabled() {
+      return this.name.trim().length < 3;
     }
   },
   methods: {
     ...mapActions(['checkName']),
     ...mapMutations(['setName']),
     submitName() {
-      if (this.name.trim() != '') {
+      if (!this.isDisabled) {
         this.checkName()
       }
+    },
+    formatName(value) {
+      return value.replace(/[^a-zA-Z]/g, '');
     }
   }
 }
